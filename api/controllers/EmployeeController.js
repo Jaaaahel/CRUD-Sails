4b8525534736016fdd07b4c41a89d7fa566ffa39
Jeremiah <jahelrelova@outlook.com>
2019-06-13 17:43:16 +0800

    Added select/read

--- api/controllers/EmployeeController.js
+++ api/controllers/EmployeeController.js
@@ -1,11 +1,12 @@
 module.exports = {
 
-	index (req, res) {
-		return res.view('pages/employees');
+	async index (req, res) {
+		let employees = await Employee.find();
 
+		return res.view('pages/employees', {employees});
 	},
+	
 	async add (req, res) {
-
 		let accountType = req.param('accountType');
 		let username = req.param('username');
 		let password = req.param('password');
@@ -23,7 +24,27 @@ module.exports = {
 		});
 
 		return res.redirect('/employees');
-
 		console.log(accountType, username, password, email, firstName, lastName);
-	}
+	},
+
+	async update (req, res) {
+		let accountType = req.param('accountType');
+		let username = req.param('username');
+		let password = req.param('password');
+		let email = req.param('email');
+		let firstName = req.param('firstName');
+		let lastName = req.param('lastName');
+		
+		await Employee.update({
+			accountType,
+			username,
+			password,
+			email,
+			firstName,
+			lastName
+		});
+		
+		return res.redirect('/employees');
+	},
+
 };
